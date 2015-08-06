5ea09b5887fea269be35112fa923d0919e013606
Chris McCormick <chris@mccormick.cx>
2015-08-06 20:08:49 +0800

    Update clojurescript version.

--- project.clj
+++ project.clj
@@ -18,7 +18,7 @@
                  [compojure "1.4.0"]
                  [hiccup "1.0.5"]
                  [environ "1.0.0"]
-                 [org.clojure/clojurescript "0.0-3308" :scope "provided"]
+                 [org.clojure/clojurescript "1.7.28"]
                  [com.lucasbradstreet/cljs-uuid-utils "1.0.2"]
                  [secretary "1.2.3"]]
 
